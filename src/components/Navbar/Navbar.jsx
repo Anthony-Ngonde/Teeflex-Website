c57acf5aa6c009ffabddadc569a02b72dea54541
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-18 13:24:05 +0300

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../../assets/teeflex-logo.png';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <img src={logo} alt="Teeflex Fitness Logo" />
+          <span>Teeflex Fitness</span>
+        </div>
+        <ul className="navbar-menu">
+          <li><a href="#home">HOME</a></li>
+          <li><a href="#program">PROGRAM</a></li>
+          <li><a href="#membership">MEMBERSHIP</a></li>
+          <li><a href="#trainers">TRAINERS</a></li>
+          <li><a href="#about">ABOUT US</a></li>
+          <li><a href="#contact">CONTACT US</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
