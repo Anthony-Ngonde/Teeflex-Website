953891e974a2ead39008e8f9ff7914bc99e48b04
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-20 12:44:10 +0300

    Added youtube icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -38,7 +38,8 @@ function Footer() {
             <a href="#"><i className="fab fa-facebook-f"></i></a>
             <a href="#"><i className="fab fa-instagram"></i></a>
             <a href="#"><i className="fab fa-twitter"></i></a>
-            <a href="#"><i className="fab fa-tiktok"></i></a> {/* Added TikTok Icon */}
+            <a href="#"><i className="fab fa-tiktok"></i></a>
+            <a href="#"><i className="fab fa-youtube"></i></a>
           </div>
         </div>
         <div className="footer-links">
