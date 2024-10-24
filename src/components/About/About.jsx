bf7a2f2e1f96256c1c05517589eab3b8b803e143
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-24 14:37:31 +0300

    Fixed the about section text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,8 +11,8 @@ function About() {
         </div>
         <div className="about-content">
           <h2>About</h2>
-          <p>At Teeflex Gym and Fitness Centre, we're dedicated to helping you achieve your fitness goals and lead a healthier lifestyle. Our state-of-the-art facilities, expert trainers, and diverse range of programs cater to all fitness levels and interests.</p>
-          <p>Whether you're just starting your fitness journey or you're a seasoned athlete, we provide the support, guidance, and motivation you need to succeed. Join our community and experience the Teeflex difference today!</p>
+          <p>At Teeflex Gym and Fitness Centre, we are dedicated to helping you achieve your fitness goals and lead a healthier lifestyle. Our state-of-the-art facilities, expert trainer and diverse range of programs cater to all fitness levels and interests.</p>
+          <p>Whether you are just starting your fitness journey or you are a seasoned athlete, we provide the support, guidance and motivation you need to succeed. Join our community and experience the Teeflex difference today!</p>
         </div>
       </div>
     </section>
