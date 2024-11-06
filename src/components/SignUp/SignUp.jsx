35790228b1dfc5004061d2e2fc80a2d8fe604f83
Anthony Ngonde <anto.ngonde@gmail.com>
2024-11-06 13:14:07 +0300

    Adding validation to our registration form

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,69 +1,74 @@
 import React, { useState } from 'react';
 import { Form, Button, FormControl, FormGroup, FormLabel } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
 import './SignUp.css';
 
 const SignUp = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+    const { register, handleSubmit, reset, formState: { errors }, watch } = useForm();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Add your form submission logic here
-        console.log("Form submitted with:", { name, email, password, confirmPassword });
+
+
+    const submitForm = (data) => {
+
+        console.log(data);
+
+        reset()
     };
+
+    console.log(watch("name"));
+    console.log(watch("email"));
+    console.log(watch("password"));
+    console.log(watch("confirmPasword"));
 
     return (
         <div className='container'>
             <div className='form'>
                 <h1>Registration Page</h1>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit(submitForm)}>
                     <FormGroup>
                         <FormLabel>Name</FormLabel>
                         <FormControl
                             type='text'
                             placeholder='Your name'
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            name="name"
+                            {...register("name", { required: true, maxLength: 25 })}
                         />
+                        
                     </FormGroup>
-
+                    {errors.name && <span style={{color:"red"}}>Name is required</span>}
+                    <br />
                     <FormGroup>
                         <FormLabel>Email</FormLabel>
                         <FormControl
                             type='email'
                             placeholder='Your email'
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            name="email"
+                            {...register("email", { required: true, maxLength: 80 })}
                         />
+                        
                     </FormGroup>
-
+                    {errors.email && <span style={{color:"red"}}>Email is required</span>}
+                    <br />
                     <FormGroup>
                         <FormLabel>Password</FormLabel>
                         <FormControl
                             type='password'
                             placeholder='Your password'
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            name="password"
+                            {...register("password", { required: true, minLength: 8 })}
                         />
+                        
                     </FormGroup>
-
+                    {errors.password && <span style={{color:"red"}}>Password must be at least 8 characters</span>}
+                    <br />
                     <FormGroup>
                         <FormLabel>Confirm Password</FormLabel>
                         <FormControl
                             type='password'
-                            placeholder='Your password'
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                            name="confirmPassword"
+                            placeholder='Confirm your password'
+                            {...register("confirmPassword", { required: true, minLength: 8 })}
                         />
+                        
                     </FormGroup>
-                    <br></br>
-
+                    {errors.confirmPassword && <span style={{color:"red"}}>Password must be at least 8 characters</span>}
+                    <br />
                     <FormGroup>
                         <Button type='submit' variant='primary'>Sign Up</Button>
                     </FormGroup>
